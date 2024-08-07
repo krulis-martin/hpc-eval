96749d0737ba276252bf229a90ea857ccc2430e3
Martin Kruliš <krulis@d3s.mff.cuni.cz>
2024-08-07 10:55:44 +0200

    Initializing the project with toml specification.

---
+++ src/hpc-eval/main.py
@@ -0,0 +1,11 @@
+import sys
+
+
+def main():
+    """
+    Main entry point of HPC-eval CLI tool.
+    """
+    sys.stdin.reconfigure(encoding='utf-8')
+    sys.stdout.reconfigure(encoding='utf-8')
+    sys.stderr.reconfigure(encoding='utf-8')
+    print("And we are done here....")
